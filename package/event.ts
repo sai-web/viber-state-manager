662f30ab6cf4f0e515d7d40ea2fe3fe4a1e52892
saisumith770 <saisumith812@gmail.com>
2020-12-11 14:23:29 +0400

    created an interfaces file

---
+++ package/event.ts
@@ -0,0 +1,50 @@
+import { EventConfig } from './interfaces'
+
+export class Event {
+    private _callbacks: Function[]
+    private _uses: number
+    constructor(private config: EventConfig) {
+        if (this.config.disableAfterUses) this._uses = 0
+    }
+
+    //attach a new callback
+    public on(callback: () => void) {
+        if (!this.config.active) { console.error(`Disabled event was attached a callback`); return }
+        const cleanUpFunc = () => {
+            this.unsub(callback)
+        }
+        if (this.config.maxSubs && this._callbacks.length === this.config.maxSubs) return cleanUpFunc
+        if (this.config.disableAfterUses && this.config.disableAfterUses < this._uses) {
+            this.disable()
+            return cleanUpFunc
+        }
+        this._callbacks.push(callback)
+        return cleanUpFunc
+    }
+
+    //call all the callbacks and increment the uses
+    public emit() {
+        if (!this.config.active) { console.error(`Disabled event was emitted`); return }
+        this._callbacks.forEach(callback => {
+            callback()
+        })
+        if (this.config.disableAfterUses) this._uses++
+    }
+    public disable() {
+        this.config.active = false
+    }
+    public onNext(callback: () => void) {
+        if (!this.config.active) { console.error(`Disabled event was attached a callback`); return }
+        this._callbacks.push(() => {
+            callback()
+            this.unsub(callback)
+        })
+    }
+
+    //filter the callbacks to remove certain callbacks
+    private unsub(callback: Function) {
+        this._callbacks = this._callbacks.filter(emitter => {
+            return emitter !== callback
+        })
+    }
+}
---
+++ package/interfaces.ts
@@ -0,0 +1,33 @@
+//types and interfaces for states
+export interface StateClass<ValueType> {
+    name: string
+    key: (name: string) => this
+    value: ValueType
+    set: (value: ValueType | setStateFunc<ValueType>) => null | this
+    exists: () => boolean
+    patch: (patchableObject: Object) => this
+    watch: (name: string, watcher: Function | Function[]) => this
+    removeWatcher: (name: string) => this
+    onNext: (NextFunc: Function) => this
+    is: (value: any) => boolean
+    isNot: (value: any) => boolean
+    reset: () => this
+    undo: () => this
+}
+
+export type setStateFunc<ValueType> = (state: ValueType) => ValueType
+
+export type InternalInformation<ValueType> = {
+    name?: string
+    value: ValueType
+    initialValue: ValueType
+    previousState: ValueType
+}
+
+//types and interfaces for events
+export interface EventConfig {
+    name?: string
+    maxSubs?: number
+    active: boolean
+    disableAfterUses?: number
+}
