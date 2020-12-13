3202dbbf8e9ae514b2deeb26c5e028c8eab7e852
saisumith770 <saisumith812@gmail.com>
2020-12-13 18:15:08 +0400

    no more redundant copying of data

--- package/collections/group.ts
+++ package/collections/group.ts
@@ -4,20 +4,18 @@ type PrimaryKey = string | number
 
 export class Group<DataType> extends State<PrimaryKey[]>{
     private _indexes: PrimaryKey[]
-    private _data: DataType[]
 
     public get index() {
         return this._indexes
     }
 
     public get output() {
-        return this._data
+        return this.collection.getDataFromKeys(this._indexes)
     }
 
     constructor(public collection: Collection<DataType>, initalIndex: PrimaryKey[] = [], config: { name?: string } = {}, public primaryKey: PrimaryKey) {
         super(initalIndex)
         if (config.name) this.key(config.name)
-        this._data = collection.getDataFromKeys(initalIndex)
     }
 
     public add(indexes: PrimaryKey | PrimaryKey[]) {
@@ -25,7 +23,6 @@ export class Group<DataType> extends State<PrimaryKey[]>{
         indexes.forEach(index => {
             if (!(index in this._indexes)) {
                 this._indexes.push(index)
-                this.collection.getDataFromKeys([index]).forEach(data => this._data.push(data))
             }
         })
         return this;
@@ -36,14 +33,12 @@ export class Group<DataType> extends State<PrimaryKey[]>{
         indexes.forEach(index => {
             if (index in this._indexes) {
                 this._indexes = this._indexes.filter(key => (key !== index))
-                this._data = this._data.filter(item => (item[this.primaryKey] !== index))
             }
         })
         return this;
     }
 
     public reset() {
-        this._data = []
         this._indexes = []
         return this
     }
